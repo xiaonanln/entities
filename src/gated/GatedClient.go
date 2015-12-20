a8f3c105732befc547fb6887a3d139fe2a619266
seis <xiaonanln@gmail.com>
2015-12-21 01:10:19 +0800

    no message

---
+++ src/gated/GatedClient.go
@@ -0,0 +1,4 @@
+package gated
+
+type GatedClient struct {
+}
---
+++ src/gated/GatedClientProxy.go
@@ -0,0 +1,10 @@
+package gated
+
+import "net"
+
+type GatedClientProxy struct {
+}
+
+func NewGatedClientProxy(conn net.Conn) *GatedClientProxy {
+	return nil
+}
